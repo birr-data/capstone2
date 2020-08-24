185215e0f28b7f091e31ab469d93eb82b8ccb1fd
Manuel J. Gomez <birr.data@gmail.com>
2020-08-24 13:15:12 -0500

    First commit.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Springboard Data Science Program Capstone 2 Project',
+    author='Manuel J Gomez',
+    license='',
+)
